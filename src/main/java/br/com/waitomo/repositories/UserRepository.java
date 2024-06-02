6a51fba4cb25745baf769774d4e79d6761551e9e
Namibo06 <namibo654@gmail.com>
2024-06-02 11:58:06 -0300

    feat:UserRepository

---
+++ src/main/java/br/com/waitomo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.waitomo.repositories;
+
+import br.com.waitomo.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel,Long> {
+}
