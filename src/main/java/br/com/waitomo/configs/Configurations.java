9a1cb0f84378ccd39e2fc97d63cbe9748841bb07
Namibo06 <namibo654@gmail.com>
2024-06-02 11:58:28 -0300

    feat:Configurations

---
+++ src/main/java/br/com/waitomo/configs/Configurations.java
@@ -0,0 +1,19 @@
+package br.com.waitomo.configs;
+
+import br.com.waitomo.dtos.TaskDTO;
+import br.com.waitomo.models.TaskModel;
+import br.com.waitomo.models.UserModel;
+import br.com.waitomo.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.modelmapper.ModelMapper;
+
+@Configuration
+public class Configurations {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
